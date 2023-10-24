46eb49027e2d55e8b6ab1f96c6d5652b2017c80a
benja27 <50673336+benja27@users.noreply.github.com>
2023-10-24 17:26:04 -0600

    allow iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :allow_iframe
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private 
+
+  def allow_iframe
+    response.headers.except!('X-Frame-Options')
   end
 end
